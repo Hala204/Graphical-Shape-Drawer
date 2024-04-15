c9b17a634081426e027013d3306d34bb23ff5f1b
Hala204 <117945870+Hala204@users.noreply.github.com>
2024-04-15 12:56:00 +0200

    Update with the rocket composite shape

--- main.cpp
+++ main.cpp
@@ -41,6 +41,62 @@ void DrawIceCream(window& w, color c, int rx, int ry, int size)
 
 
 
+
+
+void DrawRocket(window& w, color c, int rx, int ry, int size)
+{
+	int windowWidth = w.GetWidth();
+	int windowHeight = w.GetHeight();
+	ry = 1 * size + rand() % (windowHeight - 50 - 5 * size + 1);
+
+	int NoseConeWidth = size;
+	int NoseConeHeight = NoseConeWidth;
+	int RectHeight = 4 * size;
+
+	int XaxisLeftPoint = rx - (NoseConeWidth / 2);
+	int XaxisRightPoint = rx + (NoseConeWidth / 2);
+	int YaxisLeft_RightPoint = ry + NoseConeHeight;
+	int RectYaxisBottomRightPoint = YaxisLeft_RightPoint + RectHeight;
+
+	XaxisLeftPoint = max(0, min(XaxisLeftPoint, windowWidth));
+	XaxisRightPoint = max(0, min(XaxisRightPoint, windowWidth));
+	YaxisLeft_RightPoint = max(0, min(YaxisLeft_RightPoint, windowHeight));
+	RectYaxisBottomRightPoint = max(0, min(RectYaxisBottomRightPoint, windowHeight));
+
+	w.SetPen(BLACK, 5);
+	w.SetBrush(RED);
+	w.DrawTriangle(XaxisLeftPoint, YaxisLeft_RightPoint, rx, ry, XaxisRightPoint, YaxisLeft_RightPoint);
+
+	w.SetPen(BLACK, 5);
+	w.SetBrush(GREY);
+	w.DrawRectangle(XaxisLeftPoint, YaxisLeft_RightPoint, XaxisRightPoint, RectYaxisBottomRightPoint, FILLED);
+
+	w.SetPen(BLACK, 5);
+	w.SetBrush(RED);
+	int NewTriangleBaseAboveXaxis1 = XaxisRightPoint;  //The Point above
+	int NewTriangleBaseAboveYaxis1 = RectYaxisBottomRightPoint - size - 25;  //The Point above
+	int NewTriangleBaseBelowXaxis1 = XaxisRightPoint;  //The point below
+	int NewTriangleBaseBelowYaxis1 = RectYaxisBottomRightPoint - 25; //The point below
+	int NewTriangleNoseConeXaxis1 = XaxisRightPoint + (2 * size);
+	int NewTriangleNoseConeYaxis1 = RectYaxisBottomRightPoint - 25;
+	w.DrawTriangle(NewTriangleBaseAboveXaxis1, NewTriangleBaseAboveYaxis1, NewTriangleNoseConeXaxis1, NewTriangleNoseConeYaxis1, NewTriangleBaseBelowXaxis1, NewTriangleBaseBelowYaxis1, FILLED);
+
+	w.SetPen(BLACK, 5);
+	w.SetBrush(RED);
+	int NewTriangleBaseAboveXaxis2 = XaxisLeftPoint;  //The Point above
+	int NewTriangleBaseAboveYaxis2 = RectYaxisBottomRightPoint - size - 25;  //The Point above
+	int NewTriangleBaseBelowXaxis2 = XaxisLeftPoint;  //The point below
+	int NewTriangleBaseBelowYaxis2 = RectYaxisBottomRightPoint - 25; //The point below
+	int NewTriangleNoseConeXaxis2 = XaxisLeftPoint - (2 * size);
+	int NewTriangleNoseConeYaxis2 = RectYaxisBottomRightPoint - 25;
+	w.DrawTriangle(NewTriangleBaseAboveXaxis2, NewTriangleBaseAboveYaxis2, NewTriangleNoseConeXaxis2, NewTriangleNoseConeYaxis2, NewTriangleBaseBelowXaxis2, NewTriangleBaseBelowYaxis2, FILLED);
+}
+
+
+
+
+
+
 int main() {
 	int hight, width, sh, sw;
 	hight = 700;
@@ -61,10 +117,11 @@ int main() {
 	w.SetPen(BLACK, 70);
 	w.SetFont(15, BOLD, BY_NAME, "Arial");
 	w.DrawString(15 + 170, w.GetHeight() - 40, "press (i) to draw ice cream ||");
+	w.DrawString(15 + 380, w.GetHeight() - 40, "press (r) to draw rocket ||");
 	w.DrawString(15 + 1100, w.GetHeight() - 40, "press (q) to clear Screen ||");
 
 
-	int rx, ry, size,length;
+	int rx, ry, size, length;
 
 	POINT upLeft;
 
@@ -74,7 +131,7 @@ int main() {
 
 		size = rand() % hight / 10;
 		rx = 2 * size + rand() % (width - 2 * size - 2 * size + 1);
-		ry = 2 * size + rand() % (hight - 50 - 5 * size  + 1);
+		ry = 2 * size + rand() % (hight - 50 - 5 * size + 1);
 
 		if (key == 'q' || key == 'Q')
 		{
@@ -90,6 +147,7 @@ int main() {
 			w.SetPen(BLACK, 70);
 			w.SetFont(15, BOLD, BY_NAME, "Arial");
 			w.DrawString(15 + 170, w.GetHeight() - 40, "press (i) to draw ice cream ||");
+			w.DrawString(15 + 380, w.GetHeight() - 40, "press (r) to draw rocket ||");
 			w.DrawString(15 + 1100, w.GetHeight() - 40, "press (q) to clear Screen ||");
 
 		}
@@ -99,6 +157,13 @@ int main() {
 			w.FlushMouseQueue();
 			DrawIceCream(w, BROWN, rx, ry, size);
 		}
+		else if (key == 'r' || key == 'R')
+		{
+			w.FlushKeyQueue();
+			w.FlushMouseQueue();
+			DrawRocket(w, RED, rx, ry, size);
+		}
+		
 	} while (key != 'e');
 
 	return 0;
